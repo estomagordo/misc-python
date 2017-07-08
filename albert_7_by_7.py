1f214416a904b6416c98e555af4c01bc3e94a29c
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-07-08 11:07:34 +0200

    Massive speedup due to optimal solution always being inner square

--- albert_7_by_7.py
+++ albert_7_by_7.py
@@ -1,15 +1,18 @@
 from sys import argv
 from itertools import combinations
 
+def is_inner_square(n):
+    return n > 6 and n < 42 and not n%7 in [0,6]
+
 def multiply_between(start, end):
     return reduce(lambda x,y: x*y, range(start, end+1))
 
-tiles = [x for x in xrange(49) if not x in [0,6,42,48]]
+tiles = [x for x in xrange(49) if is_inner_square(x)]
 
 setlist = [set([x, x+6, x+7, x+8, x+14]) for x in xrange(35) if not x%7 in [0,6]]
     
 def look_for_of_size(n):
-    maxperms = multiply_between(46-n, 45) / multiply_between(2, n)
+    maxperms = multiply_between(len(tiles)+1-n, len(tiles)) / multiply_between(2, n)
     count = 0
     working = []
     
