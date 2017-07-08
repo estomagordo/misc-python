c2e451407a15337849dda1b12829ab9f7b1a15ac
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-07-08 09:32:46 +0200

    Solver for Alberts Kluringar 7x7 puzzle 2017-07-07

---
+++ albert_7_by_7.py
@@ -0,0 +1,31 @@
+from sys import argv
+from itertools import combinations
+from random import shuffle
+
+def multiply_between(start, end):
+    return reduce(lambda x,y: x*y, range(start, end+1))
+
+tiles = [x for x in xrange(49) if not x in [0,6,42,48]]
+
+setlist = [set([x, x+6, x+7, x+8, x+14]) for x in xrange(35) if not x%7 in [0,6]]
+    
+def look_for_of_size(n):
+    maxperms = multiply_between(46-n, 45) / multiply_between(2, n)
+    count = 0
+    working = []
+    shuffle(tiles)
+    
+    for c in combinations(tiles, n):
+        if all(any(x in s for x in c) for s in setlist):
+            working.append(c)
+        count += 1
+        if count % 10000 == 0:
+            print '%0.2f' % (100.0 * float(count) / float(maxperms)) + '%'
+            
+    return working
+        
+if __name__ == '__main__':
+    n = int(argv[1])
+    working = look_for_of_size(n)
+    print working
+    print len(working)
