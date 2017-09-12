4359e3bee613cbe5bdc224937a3520888fb55529
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-09-12 22:30:24 +0200

    final print statement, implementation of order_valid()

--- chess960.py
+++ chess960.py
@@ -4,7 +4,25 @@ def mate_in_two(configuration):
 
 
 def order_valid(order):
+    rook_count = 0
+    king_valid = True
+    blackbish = False
+    whitebish = False
     
+    for x in xrange(len(order)):
+        c = order[x]
+        
+        if c == 'K':
+            king_valid = rook_count == 1
+        if c == 'R':
+            rook_count += 1
+        if c == 'B':
+            if x % 2 == 0:
+                blackbish = True
+            else:
+                whitebish = True
+                
+    return king_valid and blackbish and whitebish
 
 mate_count = 0
 backpieces = 'KQRRNNBB'
@@ -12,3 +30,5 @@ backpieces = 'KQRRNNBB'
 for p in permutations(backpieces):
     if order_valid(p):
         mate_count += mate_in_two(p)
+        
+print mate_count
